25f6684110f784445e861ccbfebbc03aeccca777
FlynntKnapp <bruce.stull@gmail.com>
2024-03-07 18:11:22 -0500

    In progress

--- gpio_server.py
+++ gpio_server.py
@@ -7,15 +7,43 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 
+home = "<a href='/gpio/'>Home</a>"
+
+form = """
+	<form action="./on">
+		<input type="submit" value="Pin 17 Up" />
+	</form>
+	<form action="./off">
+		<input type="submit" value="Pin 17 Down" />
+	</form>
+"""
+
+@app.route('/gpio/')
+def gpio_home():
+    return home + "<br>" + form
+
 @app.route('/gpio/on')
 def gpio_on():
     GPIO.output(17, GPIO.HIGH)
-    return 'GPIO 17 turned on'
+    return (
+	f"{home}"
+	f"<br>"
+	f"GPIO 17 turned on"
+        f"{form}"
+    )
 
 @app.route('/gpio/off')
 def gpio_off():
     GPIO.output(17, GPIO.LOW)
-    return 'GPIO 17 turned off'
+    return (
+	f"{home}"
+	f"<br>"
+	f"GPIO 17 turned off"
+	f"{form}"
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
+
