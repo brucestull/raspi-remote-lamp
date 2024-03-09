931cd16fd43a418c2e84a45c199a966972c8b2a6
FlynntKnapp <bruce.stull@gmail.com>
2024-03-09 03:43:30 -0500

    Add LED blinks for connection and request send status

--- raspi-pico/main.py
+++ raspi-pico/main.py
@@ -3,10 +3,20 @@ import urequests
 import json
 from time import sleep
 from machine import Pin
+from picozero import pico_led
 
 
 # Setup pin 18 as input:
 pin18 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+
+
+# Function to fast-blink `pico_led`:
+def pico_led_fast_blink(cycles):
+    for _ in range(cycles):
+        pico_led.on()
+        sleep(.1)
+        pico_led.off()
+        sleep(.1)
 
 # Function to load WiFi credentials from `config.json`:
 def load_config(file_path):
@@ -24,10 +34,12 @@ config = load_config("config.json")
 ssid = config["ssid"]
 password = config["password"]
 print("Loaded WiFi Settings!")
+pico_led_fast_blink(10)
 sleep(0.25)
 
-url_on = 'http://192.168.1.91:8000/gpio/on'
-url_off = 'http://192.168.1.91:8000/gpio/off'
+
+url_on = 'http://192.168.4.1:8000/gpio/on'
+url_off = 'http://192.168.4.1:8000/gpio/off'
 
 # Setup WiFi connection
 wlan = network.WLAN(network.STA_IF)
@@ -46,9 +58,13 @@ def handle_pin_change(pin):
         # Pin went high, turn on the LED
         print("Sending request to turn on LED")
         urequests.get(url_on).close()
+        pico_led_fast_blink(5)
     else:
         # Pin went low, turn off the LED
         print("Sending request to turn off LED")
         urequests.get(url_off).close()
+        pico_led.on()
+        sleep(1)
+        pico_led.off()
 
 pin18.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=handle_pin_change)
