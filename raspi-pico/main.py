c8fefac4fe9f98df323220ff305bf306720792b0
FlynntKnapp <bruce.stull@gmail.com>
2024-03-10 21:02:26 -0400

    Remote Pin is controlled by switch on Pico.

--- raspi-pico/main.py
+++ raspi-pico/main.py
@@ -1,22 +1,21 @@
 import network
 import urequests
 import json
-from time import sleep
-from machine import Pin
+from time import sleep, ticks_ms, ticks_diff
+from machine import Pin, reset
 from picozero import pico_led
+import utime
 
 
-# Setup pin 18 as input:
-pin18 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+# Setup pin 18 (Request-send pin) as input:
+request_switch = Pin(18, Pin.IN, Pin.PULL_DOWN)
 
+# Setup pin 16 (Reset pin) as input:
+restart_button = Pin(16, Pin.IN, Pin.PULL_UP)
 
-# Function to fast-blink `pico_led`:
-def pico_led_fast_blink(cycles):
-    for _ in range(cycles):
-        pico_led.on()
-        sleep(.1)
-        pico_led.off()
-        sleep(.1)
+# Setup pin 17 as restart status LED:
+restart_status_led = Pin(17, Pin.OUT)
+
 
 # Function to load WiFi credentials from `config.json`:
 def load_config(file_path):
@@ -28,6 +27,15 @@ def load_config(file_path):
     return config
 
 
+# Function to fast-blink `pico_led`:
+def led_fast_blink(led, cycles):
+    for _ in range(cycles):
+        led.on()
+        sleep(0.1)
+        led.off()
+        sleep(0.1)
+
+
 # Get the configuration settings from the config.json file:
 config = load_config("config.json")
 # Set the ssid and password from the configuration settings:
@@ -36,8 +44,8 @@ password = config["password"]
 print("Loaded WiFi Settings!")
 
 
-url_on = 'http://192.168.4.1:8000/gpio/on'
-url_off = 'http://192.168.4.1:8000/gpio/off'
+url_on = "http://192.168.4.1:8000/gpio/on"
+url_off = "http://192.168.4.1:8000/gpio/off"
 
 # Setup WiFi connection
 wlan = network.WLAN(network.STA_IF)
@@ -49,15 +57,16 @@ while not wlan.isconnected():
     pass
 
 print("Connected to WiFi!")
-pico_led_fast_blink(10)
+led_fast_blink(pico_led, 10)
 
 
-def handle_pin_change(pin):
+# Define a callback function to send the request:
+def send_request(pin):
     if pin.value() == 1:
         # Pin went high, turn on the LED
         print("Sending request to turn on LED")
         urequests.get(url_on).close()
-        pico_led_fast_blink(5)
+        led_fast_blink(pico_led, 5)
     else:
         # Pin went low, turn off the LED
         print("Sending request to turn off LED")
@@ -66,4 +75,16 @@ def handle_pin_change(pin):
         sleep(1)
         pico_led.off()
 
-pin18.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=handle_pin_change)
+
+# Define a callback function to reset the Pico:
+def restart_pico(pin):
+    print("We're trying to restart this thing...")
+    led_fast_blink(restart_status_led, 4)
+    sleep(0.5)
+    reset()
+
+
+# Attach interrupt to Request-send pin:
+request_switch.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=send_request)
+# Attach interrupt to Restart pin:
+restart_button.irq(trigger=Pin.IRQ_FALLING, handler=restart_pico)
