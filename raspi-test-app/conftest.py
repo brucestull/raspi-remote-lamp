de9830f618416d78823f6938d89231c56a93be0e
FlynntKnapp <bruce.stull@gmail.com>
2024-03-13 14:34:57 -0400

    Fix errors in tests

--- raspi-test-app/conftest.py
+++ raspi-test-app/conftest.py
@@ -7,7 +7,8 @@ from app import app as flask_app
 @pytest.fixture(autouse=True)
 def mock_rpi_gpio(monkeypatch):
     mock_gpio = MagicMock()
-    monkeypatch.setattr("RPi.GPIO", mock_gpio)
+    # Patch 'hardware_control.GPIO' instead of 'RPi.GPIO'
+    monkeypatch.setattr("hardware_control.GPIO", mock_gpio)
     return mock_gpio
 
 
