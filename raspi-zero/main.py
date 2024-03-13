fd1653744d68dc1da0f63e36e7b8c68da3331b5e
FlynntKnapp <bruce.stull@gmail.com>
2024-03-13 12:48:00 -0400

    Refactor code

--- raspi-zero/main.py
+++ raspi-zero/main.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-lamp_led_pin = 17
+# BCM pin number for the lamp control:
+lamp_control_pin = 17
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(lamp_led_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(lamp_control_pin, GPIO.OUT, initial=GPIO.LOW)
 
 home_link = "<a href='/gpio/'>Home</a>"
 
@@ -20,45 +21,41 @@ form = """
 	</form>
 """
 
+
 @app.route("/")
 def home():
-	return redirect(url_for("gpio_home"))
+    return redirect(url_for("gpio_home"))
+
 
 @app.route("/lamp-status/")
 def lamp_status():
-    lamp_pin_status_bin = GPIO.input(lamp_led_pin)
+    lamp_pin_status_bin = GPIO.input(lamp_control_pin)
     if lamp_pin_status_bin == 0:
         lamp_pin_status_str = "OFF"
     elif lamp_pin_status_bin == 1:
         lamp_pin_status_str = "ON"
     else:
         lamp_pin_status_str = "UNKNOWN"
-    test_data = { "lamp_pin_status": lamp_pin_status_str}
-    return jsonify(test_data)
+    data = {"lamp_pin_status": lamp_pin_status_str}
+    return jsonify(data)
 
-@app.route('/gpio/')
+
+@app.route("/gpio/")
 def gpio_home():
-    return home_link + "<br>" + form
+    return f"{home_link} <br> {form}"
 
-@app.route('/gpio/on')
+
+@app.route("/gpio/on")
 def gpio_on():
-    GPIO.output(lamp_led_pin, GPIO.HIGH)
-    return (
-	f"{home_link}"
-	f"<br>"
-	f"GPIO 17 turned on"
-        f"{form}"
-    )
+    GPIO.output(lamp_control_pin, GPIO.HIGH)
+    return f"{home_link} <br> GPIO 17 turned on {form}"
 
-@app.route('/gpio/off')
+
+@app.route("/gpio/off")
 def gpio_off():
-    GPIO.output(lamp_led_pin, GPIO.LOW)
-    return (
-	f"{home_link}"
-	f"<br>"
-	f"GPIO 17 turned off"
-	f"{form}"
-    )
+    GPIO.output(lamp_control_pin, GPIO.LOW)
+    return f"{home_link} <br> GPIO 17 turned off {form}"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
