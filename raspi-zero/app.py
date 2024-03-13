26c17605518f1ad12a50a5f5821593f379d1aa30
FlynntKnapp <bruce.stull@gmail.com>
2024-03-13 16:03:18 -0400

    Implemented 'hardware_control.py'.

--- raspi-zero/app.py
+++ raspi-zero/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, redirect, url_for, jsonify
 import RPi.GPIO as GPIO
+from hardware_control import HardwareControl
 
 app = Flask(__name__)
 
 # BCM pin number for the lamp control:
 lamp_control_pin = 17
 
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(lamp_control_pin, GPIO.OUT, initial=GPIO.LOW)
+# Initialize the hardware control:
+hardware = HardwareControl(lamp_control_pin)
 
 home_link = "<a href='/gpio/'>Home</a>"
 
@@ -29,32 +29,27 @@ def home():
 
 @app.route("/lamp-status/")
 def lamp_status():
-    lamp_pin_status_bin = GPIO.input(lamp_control_pin)
-    if lamp_pin_status_bin == 0:
-        lamp_pin_status_str = "OFF"
-    elif lamp_pin_status_bin == 1:
-        lamp_pin_status_str = "ON"
-    else:
-        lamp_pin_status_str = "UNKNOWN"
+    lamp_pin_status_str = hardware.get_lamp_pin_status()
     data = {"lamp_pin_status": lamp_pin_status_str}
     return jsonify(data)
 
 
 @app.route("/gpio/")
 def gpio_home():
-    return f"{home_link} <br> {form}"
+    lamp_pin_status_str = hardware.get_lamp_pin_status()
+    return f"{home_link} <br> Lamp is: {lamp_pin_status_str} {form}"
 
 
 @app.route("/gpio/on")
 def gpio_on():
-    GPIO.output(lamp_control_pin, GPIO.HIGH)
-    return f"{home_link} <br> GPIO 17 turned on {form}"
+    response = hardware.turn_lamp_on()
+    return f"{home_link} <br> {response} {form}"
 
 
 @app.route("/gpio/off")
 def gpio_off():
-    GPIO.output(lamp_control_pin, GPIO.LOW)
-    return f"{home_link} <br> GPIO 17 turned off {form}"
+    response = hardware.turn_lamp_off()
+    return f"{home_link} <br> {response} {form}"
 
 
 if __name__ == "__main__":
---
+++ raspi-zero/hardware_control.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+
+
+class HardwareControl:
+    """
+    A class to control the hardware of the Raspberry Pi Zero.
+
+    Attributes:
+        lamp_control_pin (int): The BCM pin number for the lamp control.
+    """
+
+    def __init__(self, lamp_control_pin):
+        self.lamp_control_pin = lamp_control_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.lamp_control_pin, GPIO.OUT, initial=GPIO.LOW)
+
+    def get_lamp_pin_status(self):
+        """
+        Get the status of the lamp pin.
+
+        Returns:
+            str: The status of the lamp pin.
+        """
+        lamp_pin_status_binary = GPIO.input(self.lamp_control_pin)
+        if lamp_pin_status_binary == 0:
+            return "OFF"
+        elif lamp_pin_status_binary == 1:
+            return "ON"
+        else:
+            return "UNKNOWN"
+        # Alternate implementation:
+        # if lamp_pin_status_binary == 0:
+        #     lamp_pin_status_str = "OFF"
+        # elif lamp_pin_status_binary == 1:
+        #     lamp_pin_status_str = "ON"
+        # else:
+        #     lamp_pin_status_str = "UNKNOWN"
+        # return lamp_pin_status_str
+
+    def turn_lamp_on(self):
+        """
+        Turn the lamp on.
+        """
+        GPIO.output(self.lamp_control_pin, GPIO.HIGH)
+        return "Lamp turned on."
+
+    def turn_lamp_off(self):
+        """
+        Turn the lamp off.
+        """
+        GPIO.output(self.lamp_control_pin, GPIO.LOW)
+        return "Lamp turned off."
