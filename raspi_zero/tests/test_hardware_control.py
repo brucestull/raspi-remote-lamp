347e36e67ea1aa6ac2264d3b8a828883d96cd406
FlynntKnapp <bruce.stull@gmail.com>
2024-03-15 08:57:46 -0400

    Made some progress, but still has errors.

--- raspi_zero/tests/test_hardware_control.py
+++ raspi_zero/tests/test_hardware_control.py
@@ -1,27 +1,31 @@
 # tests/test_hardware_control.py
 
+import importlib
 import unittest
 from unittest.mock import patch
 
 from mocks.mock_gpio import MockGPIO
 
-# Patch 'RPi.GPIO' in sys.modules
-with patch.dict("sys.modules", {"RPi.GPIO": MockGPIO}):
-    # Now that 'RPi.GPIO' is mocked, you can safely import your module
-    from hardware_control import HardwareControl
-
 
 class TestHardwareControl(unittest.TestCase):
+    @patch.dict("sys.modules", {"RPi.GPIO": MockGPIO})
     def test_turn_lamp_on(self):
+        # Dynamically import the HardwareControl class
+        hardware_module = importlib.import_module("hardware_control")
+        HardwareControl = getattr(hardware_module, "HardwareControl")
+
         hardware = HardwareControl(17)
         hardware.turn_lamp_on()
-        # Ensure the lamp's status is "ON" after the method call
         self.assertEqual(hardware.get_lamp_pin_status(), "ON")
 
+    @patch.dict("sys.modules", {"RPi.GPIO": MockGPIO})
     def test_turn_lamp_off(self):
+        # Dynamically import the HardwareControl class
+        hardware_module = importlib.import_module("hardware_control")
+        HardwareControl = getattr(hardware_module, "HardwareControl")
+
         hardware = HardwareControl(17)
         hardware.turn_lamp_off()
-        # Ensure the lamp's status is "OFF" after the method call
         self.assertEqual(hardware.get_lamp_pin_status(), "OFF")
 
 
